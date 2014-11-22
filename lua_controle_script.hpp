3f8068dd8f6d09b2c9737966fcc1d9cc5440d5e3
zaru <zaru_of_iron@yahoo.co.jp>
2014-11-22 14:22:50 +0900

    add ControleWindow class

--- lua_controle_script.hpp
+++ lua_controle_script.hpp
@@ -5,10 +5,13 @@
 #include <memory>
 #include <string>
 
+class ControleWindow;
+
 class LuaControleScript : public ControleScript
 {
 private:
 	std::unique_ptr<lua_State> lua_;
+	std::unique_ptr<ControleWindow> window_;
 	std::string filename_;
 	Ash& ash_;
 	static LuaControleScript *thisPtr_;
@@ -17,6 +20,7 @@ public:
 	LuaControleScript(Ash& ash, const std::string& filename);
 	
 	void initialize();
+	void onCommand(int index, int id);
 };
 
 #endif
