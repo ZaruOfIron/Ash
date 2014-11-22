8ecea578b11ec5312c95264a380c7c56a4368fa1
zaru <zaru_of_iron@yahoo.co.jp>
2014-11-22 13:39:42 +0900

    add ControleScript class

---
+++ controle_script.hpp
@@ -0,0 +1,16 @@
+#ifndef ___CONTROLE_SCRIPT_HPP___
+#define ___CONTROLE_SCRIPT_HPP___
+
+class ControleScript
+{
+public:
+	ControleScript(){}
+	virtual ~ControleScript(){}
+
+	virtual void initialize() = 0;
+
+};
+
+
+#endif
+
