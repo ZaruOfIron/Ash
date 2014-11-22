2f8f9aa21aae638f738819e753d0a0b2509eb6ba
zaru <zaru_of_iron@yahoo.co.jp>
2014-11-22 13:35:25 +0900

    add Ash class

---
+++ ash.hpp
@@ -0,0 +1,20 @@
+#ifndef ___ASH_HPP___
+#define ___ASH_HPP___
+
+#include "user.hpp"
+#include <vector>
+
+class Ash
+{
+private:
+	std::vector<User> users_;
+
+public:
+	Ash();
+
+
+};
+
+
+#endif
+
