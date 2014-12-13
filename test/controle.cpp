30827f576a19bb8740c671d126589dc737a62857
ZaruOfIron <zaru_of_iron@yahoo.co.jp>
2014-12-13 11:28:50 +0900

    rename script1

--- test/controle.cpp
+++ test/controle.cpp
@@ -9,11 +9,12 @@ BOOST_AUTO_TEST_CASE( no_script )
 	BOOST_CHECK_THROW(LuaControleScript(ash, ""), LuaCantLoadFileError);
 }
 
-BOOST_AUTO_TEST_CASE( script1 )
+BOOST_AUTO_TEST_CASE( empty_script )
 {
 	Ash ash;
-	LuaControleScript controle(ash, "test/script1.lua");
+	LuaControleScript controle(ash, "test/empty_script.lua");
 	BOOST_CHECK_THROW(controle.initialize(), LuaCantCallFuncError);
 	BOOST_CHECK_THROW(controle.onCommand(0, 0), LuaCantCallFuncError);
 }
+
 
