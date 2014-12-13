bba2c3c9efc0f3564f2ffc4ad96a462bc103b27c
ZaruOfIron <zaru_of_iron@yahoo.co.jp>
2014-12-13 12:41:09 +0900

    organize files

--- test/test_controle_script.cpp
+++ test/test_controle_script.cpp
@@ -1,7 +1,7 @@
 #include <boost/test/unit_test.hpp>
-#include "../ash.hpp"
-#include "../lua_controle_script.hpp"
-#include "../lua_exception.hpp"
+#include "ash.hpp"
+#include "lua_controle_script.hpp"
+#include "lua_exception.hpp"
 
 BOOST_AUTO_TEST_CASE( no_script )
 {
@@ -12,7 +12,7 @@ BOOST_AUTO_TEST_CASE( no_script )
 BOOST_AUTO_TEST_CASE( empty_script )
 {
 	Ash ash;
-	LuaControleScript controle(ash, "test/empty_script.lua");
+	LuaControleScript controle(ash, "script/empty_script.lua");
 	BOOST_CHECK_THROW(controle.initialize(), LuaCantCallFuncError);
 	BOOST_CHECK_THROW(controle.onCommand(0, 0), LuaCantCallFuncError);
 }
