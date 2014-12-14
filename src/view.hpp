a3eb7a1fef2782bc590e8e23687cd0d6257783d1
ZaruOfIron <zaru_of_iron@yahoo.co.jp>
2014-12-14 20:47:28 +0900

    add class View

---
+++ src/view.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef ___VIEW_HPP___
+#define ___VIEW_HPP___
+
+#include "user.hpp"
+#include <string>
+
+class View
+{
+public:
+	View(){}
+	virtual ~View(){}
+
+	void initialize(int answerNumber, int winnerNumber, const std::string& title, const std::string& subtitle, int quizId) = 0;
+	virtual void sendUserModified(int index, const User& user, int modIndex) = 0;
+	virtual void sendInfo(int id) = 0;
+	virtual void sendInfo(const std::string& str) = 0;
+};
+
+#endif
+
