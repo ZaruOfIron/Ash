0bc0294041536d841d0891d1a8da20e62148f0fa
ZaruOfIron <zaru_of_iron@yahoo.co.jp>
2014-12-14 20:47:50 +0900

    class CopyDataView

---
+++ src/copydata_view.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#ifndef ___COPYDATA_VIEW_HPP___
+#define ___COPYDATA_VIEW_HPP___
+
+#include "view.hpp"
+#include <wincore.h>
+
+class CopyDataView
+{
+private:
+	HWND hTarget_;
+
+	void sendData(int id, const std::string& str);
+
+public:
+	CopyDataView(const std::string& targetTitle);
+	~CopyDataView(){}
+
+	bool isTargetValid();
+	void initialize(int answerNumber, int winnerNumber, const std::string& title, const std::string& subtitle, int quizId);
+	void sendUserModified(int index, const User& user, int modIndex);
+	void sendInfo(int id);
+	void sendInfo(const std::string& str);
+};
+
+
+
+#endif
+
