cb5d756757092bcced5c496255b74363b4a73003
ZaruOfIron <zaru_of_iron@yahoo.co.jp>
2014-12-14 16:40:57 +0900

    add class ControleWindowFrame and ControleWindow

--- src/controle_window.hpp
+++ src/controle_window.hpp
@@ -2,13 +2,43 @@
 #define ___CONTROLE_WINDOW_HPP___
 
 #include <wincore.h>
+#include <memory>
+#include <string>
+#include <vector>
 
 class LuaControleScript;
+class ControleWindow;
+
+struct ButtonData
+{
+	int id;
+	std::string caption;
+
+	ButtonData(int id_, const std::string& caption_);
+};
+
+class ControleWindowFrame : private CWinApp
+{
+private:
+	LuaControleScript *controler_;
+	std::unique_ptr<ControleWindow> window_;
+	std::vector<ButtonData> userForm_, systemForm_;
+
+public:
+	friend ControleWindow;
+
+	ControleWindowFrame(LuaControleScript *controler_);
+
+	void registerUserButton(const ButtonData& data);
+	void registerSystemButton(const ButtonData& data);
+	
+	void run(int answer);
+};
 
 class ControleWindow : public CWnd
 {
 private:
-	LuaControleScript *controler_;
+	ControleWindowFrame *frame_;
 
 	enum {
 		FORM_PART_WIDTH = 100,
@@ -17,14 +47,15 @@ private:
 
 	void setClientSize(int width, int height);
 
-public:
-	ControleWindow(LuaControleScript *controler);
-
+	// message crackers
 	void OnCreate();
-	void OnInitialUpdate();
+	//void OnInitialUpdate();
 	void OnDestroy();
 	BOOL OnCommand(WPARAM wParam, LPARAM lParam);
 
+public:
+	ControleWindow(ControleWindowFrame *frame);
+	
 	LRESULT WndProc(UINT uMsg, WPARAM wParam, LPARAM lParam);
 };
 
