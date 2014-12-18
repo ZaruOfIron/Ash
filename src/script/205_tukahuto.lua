2cdbfbf350848a81256a7874737e53c776deed69
ZaruOfIron <zaru_of_iron@yahoo.co.jp>
2014-12-18 18:46:52 +0900

    封筒挑戦中に他のボタンの押下を無効化

--- src/script/205_tukahuto.lua
+++ src/script/205_tukahuto.lua
@@ -5,16 +5,16 @@ WINNER = 1
 CHALLENGE_CORRECT = 5
 WRONG_LIMIT = 3
 
-now_challenging = false
+challenging_index = 0
 seed_indexes = {}
 
 function export_save_data()
-	return ash_helper.serialize(ash_helper.create_twin_table('now_challenging', 'seed_indexes'))
+	return ash_helper.serialize(ash_helper.create_twin_table('challenging_index', 'seed_indexes'))
 end
 
 function import_save_data(str)
 	local data = ash_helper.deserialize(str)
-	now_challenging = data.now_challenging
+	challenging_index = data.challenging_index
 	seed_indexes = data.seed_indexes
 end
 
@@ -34,6 +34,11 @@ function initialize()
 end
 
 function on_user_button(index, id)
+	-- ••“›’§í’†‚É‚Í‚»‚Ì“šŽÒˆÈŠO‚Í‘€ì‚Å‚«‚È‚¢
+	if challenging_index ~= 0 and challenging_index ~= index then
+		return
+	end
+
 	ash.save()
 
 	local user, data, info = ash.get_user(index), {}, {}
@@ -41,24 +46,24 @@ function on_user_button(index, id)
 	if id == 1 then	-- correct
 		data.correct = user.correct + 1
 
-		if now_challenging then
+		if challenging_index ~= 0 then
 			table.insert(info, 1)
-			now_challenging = false
+			challenging_index = 0
 		else
 			if data.correct >= CHALLENGE_CORRECT then
 				if ash_helper.search_array(seed_indexes, index) then
 					table.insert(info, 1)
 				else
-					now_challenging = true
+					challenging_index = index
 					table.insert(info, 10301)
 				end
 			end
 		end
 	elseif id == 2 then	-- wrong
-		if now_challenging then
+		if challenging_index ~= 0 then
 			data.correct = CHALLENGE_CORRECT - 2
-			now_challenging = false
 			table.insert(seed_indexes, index)
+			challenging_index = 0
 		else
 			data.wrong = user.wrong + 1
 
