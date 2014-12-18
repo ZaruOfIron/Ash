628f984629e02f2513650e7e5dd6bcab5a6d1e6d
ZaruOfIron <zaru_of_iron@yahoo.co.jp>
2014-12-18 13:57:19 +0900

    add buttons to save/restore data

--- src/log_window.cpp
+++ src/log_window.cpp
@@ -1,6 +1,67 @@
 #include "log_window.hpp"
 #include "resource.h"
 #include "ash.hpp"
+#include <fstream>
+
+std::string LogWindow::askFileOpen(char *defaultExtention, char *filter, char *title)
+{
+	static OPENFILENAME ofn;
+	static char path[MAX_PATH], file[MAX_PATH];
+
+	::GetCurrentDirectory(MAX_PATH, path);
+
+	ofn.lStructSize = sizeof(ofn);
+	ofn.hwndOwner = m_hWnd;
+	ofn.lpstrInitialDir = path;
+	ofn.lpstrFilter = filter;
+	ofn.lpstrFile = file;
+	ofn.nMaxFile = MAX_PATH;
+	ofn.Flags = OFN_FILEMUSTEXIST;
+	ofn.lpstrTitle = title;
+	ofn.lpstrDefExt = defaultExtention;
+
+	if(::GetOpenFileName(&ofn))	return std::string(file);
+	return std::string("");
+}
+
+std::string LogWindow::askFileSave(char *defaultExtension, char *filter, char *title)
+{
+	static OPENFILENAME ofn;
+	static char path[MAX_PATH], file[MAX_PATH];
+
+	::GetCurrentDirectory(MAX_PATH, path);
+
+	ofn.lStructSize = sizeof(ofn);
+	ofn.hwndOwner = m_hWnd;
+	ofn.lpstrInitialDir = path;
+	ofn.lpstrFile = file;
+	ofn.nMaxFile = MAX_PATH;
+	ofn.lpstrDefExt = defaultExtension;
+	ofn.lpstrFilter = filter;
+	ofn.lpstrTitle = title;
+	ofn.Flags = OFN_PATHMUSTEXIST | OFN_OVERWRITEPROMPT;
+
+	if(::GetSaveFileName(&ofn))	return std::string(file);
+	return std::string("");
+}
+
+void LogWindow::OnSaveData()
+{
+	std::string file = askFileSave("asd", "asd(*.asd)\0*.asd\0All files(*.*)\0*.*\0\0", "Open Save Data");
+	if(file.empty())	return;
+
+	std::ofstream ofs(file);
+	ash_.writeSaveData(ofs);
+}
+
+void LogWindow::OnRestoreData()
+{
+	std::string file = askFileOpen("asd", "asd(*.asd)\0*.asd\0All files(*.*)\0*.*\0\0", "Open Save Data");
+	if(file.empty())	return;
+
+	std::ifstream ifs(file);
+	ash_.readSaveData(ifs);
+}
 
 BOOL LogWindow::OnInitDialog()
 {
@@ -15,6 +76,12 @@ BOOL LogWindow::OnCommand(WPARAM wParam, LPARAM lParam)
 	{
 	case ID_UNDO:
 		ash_.undo();
+		return TRUE;
+	case ID_SAVEDATA:
+		OnSaveData();
+		return TRUE;
+	case ID_RESTOREDATA:
+		OnRestoreData();
 		return TRUE;
 	}
 
--- src/log_window.hpp
+++ src/log_window.hpp
@@ -15,8 +15,13 @@ private:
 	Ash& ash_;
 	CEdit logEdit_;
 
+	std::string askFileOpen(char *defaultExtension, char *filter, char *title);
+	std::string askFileSave(char *defaultExtension, char *filter, char *title);
+
 	BOOL OnInitDialog();
 	BOOL OnCommand(WPARAM wParam, LPARAM lParam);
+	void OnSaveData();
+	void OnRestoreData();
 public:
 	LogWindow(Ash& ash, UINT nResID)
 		: CDialog(nResID), ash_(ash)
